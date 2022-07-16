ef8e7a61b5620f53bef8185f068cb2b400a8417a
see-why <iyadicy@gmail.com>
2022-07-16 10:48:47 +0100

    implement check_permissions method

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -73,7 +73,18 @@ def get_token_auth_header():
     return true otherwise
 '''
 def check_permissions(permission, payload):
-    raise Exception('Not Implemented')
+    if 'permissions' not in payload:
+                        raise AuthError({
+                            'code': 'invalid_claims',
+                            'description': 'Permissions not included in JWT.'
+                        }, 400)
+
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 403)
+    return True
 
 '''
 @TODO implement verify_decode_jwt(token) method
